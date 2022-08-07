a49f6b6fb8e528aaba5ce06f3b57b8da539dcc41
easyways <academy.easyways@gmail.com>
2022-08-07 22:35:49 +0100

    Update schedule.js

--- data-schema/schedule.js
+++ data-schema/schedule.js
@@ -3,10 +3,10 @@ const scheduleSchema = mongoose.Schema({
     class: { type : String},
     section: { type : String},
     time: [[{ type : String}],
-            [{ type : String}],
-            [{ type : String}],
-            [{ type : String}],
-            [{ type : String}]],
+            [{ type : String,default:'  *  '}],
+            [{ type : String,default:'  *  '}],
+            [{ type : String,default:'  *  '}],
+            [{ type : String,default:'  *  '}]],
 
 });
 module.exports = mongoose.model('Schedule', scheduleSchema);    
