4fd2e6757c1b2290076d805434267d03313c654a
easyways <academy.easyways@gmail.com>
2022-08-16 19:18:08 +0100

    activate student

--- admin-area/controllers/allstudentctrl.js
+++ admin-area/controllers/allstudentctrl.js
@@ -3,16 +3,31 @@ const cookieParser = require('cookie-parser');
 var id;
 var url = require('url');
 var urll="xx";
-
+var userlist;
 exports.rendere = (req,res,next) => {
     id=req.cookies.id;
     if(id!='010101'){
         return res.redirect('/login');
     }
     User.find({type:'Student'}).then((users)=>{
+        userlist=users;
         res.render('all-students.html',{
-            studentlist:users,
+
+            studentlist:userlist,
+            m:''
         })
     })
     
   }
+exports.save = (req,res)=>{
+    var sid=req.body.sid;
+    User.findOne({_id:sid}).then((Student)=>{
+        Student.paymentac='1';
+        Student.save().then(()=>{
+            res.render('all-students.html',{
+                studentlist:userlist,
+                m:'Student Activated!'
+            });
+        })
+    })
+}
--- admin-area/routers/allstudent.js
+++ admin-area/routers/allstudent.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const allstudentctrl = require('../controllers/allstudentctrl');
 router.get('/',allstudentctrl.rendere);
+router.post('/',allstudentctrl.save);
 module.exports = router;
