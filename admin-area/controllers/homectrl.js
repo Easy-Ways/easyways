5ae4fd69864224f77664fe4169e94d6306a9f100
Easy-Ways <academy.easyways@gmail.com>
2022-08-16 19:45:15 +0200

    14

--- admin-area/controllers/homectrl.js
+++ admin-area/controllers/homectrl.js
@@ -1,7 +1,18 @@
 const cookieparser = require('cookie-parser');
+const User = require('../data-schema/user');
+const Cour = require ('../data-schema/courses') ;
 exports.rendere = (req,res,next)=>{ 
       var id =req.cookies.id;
-        if(id=='010101'){
-        res.render('home-admin.html');    
+        if(id=='010101'){   
      }
+     User.find({type: 'Student'}).then(
+      (users) => {
+        console.log(users);
+                  res.render('home-admin.html', {
+                    studentlist: users,
+                    }); 
+                }
+              ).catch((error) => {
+            console.log(error);
+          });
     }
