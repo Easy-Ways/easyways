bd75dd92952acfdb7d6e8d01483914d854f32661
easyways <academy.easyways@gmail.com>
2022-08-16 18:54:52 +0100

    ADD-PROF

--- admin-area/controllers/homectrl.js
+++ admin-area/controllers/homectrl.js
@@ -4,15 +4,19 @@ const Cour = require ('../data-schema/courses') ;
 exports.rendere = (req,res,next)=>{ 
       var id =req.cookies.id;
         if(id=='010101'){   
-     }
-     User.find({type: 'Student'}).then(
-      (users) => {
-        console.log(users);
-                  res.render('home-admin.html', {
-                    studentlist: users,
-                    }); 
-                }
-              ).catch((error) => {
-            console.log(error);
-          });
+          User.find({type: 'Student'}).then(
+            (users) => {
+              
+                        res.render('home-admin.html', {
+                          studentlist: users,
+                          }); 
+                      }
+                    ).catch((error) => {
+                  console.log(error);
+                });
+     
+                     }else{
+                      return res.redirect('/login');
+                     }
+     
     }
--- admin-area/routers/addprof.js
+++ admin-area/routers/addprof.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const cctrl = require('../controllers/addprofctrl');
 router.get('/',cctrl.rendere);
-router.get('/',cctrl.save);
+router.post('/',cctrl.save);
 module.exports = router;
