0a3ff88bb14414388bbfa3374aea091d85f4ed3c
Easy-Ways <academy.easyways@gmail.com>
2022-08-20 23:01:17 +0200

    add

--- admin-area/controllers/editstudentctrl.js
+++ admin-area/controllers/editstudentctrl.js
@@ -1,10 +1,15 @@
 const User = require('../data-schema/user');
+const Subscription = require('../data-schema/subscriptions');
 const cookieParser = require('cookie-parser');
 var id;
 var sid;
 var url = require('url');
 var urll="xx";
 var users;
+var ms;
+var mp;
+var ys;
+var yp;
 exports.rendere = (req,res) =>{
     urll = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
     var data = url.parse(urll,true);
@@ -17,18 +22,35 @@ exports.rendere = (req,res) =>{
     if(id=='010101'){
         User.findById(sid).then(
             (userr) => {
-                userr.subscription = [];
-                const arr1 = [];
-                userr.subscription = arr1.concat(userr.subscription_m, userr.subscription_y);
-                userr.save().then(()=>{
-                    users=userr;
-                    res.render('edit-students.html',{
-                    users: userr,
-                    message:'',
-                })
-                })
-                
-            })
+                Subscription.find({section: userr.section}).then(
+                    (subs) => {
+                        if(subs[0].duration=='monthly'){
+                            ms = subs[0].subjects;
+                            mp = subs[0].prices;
+                            ys = subs[1].subjects;
+                            yp = subs[1].prices;
+                         }else{
+                            ms = subs[1].subjects;
+                            mp = subs[1].prices;
+                            ys = subs[0].subjects;
+                            yp = subs[0].prices;
+                         }
+                        userr.subscription = [];
+                        const arr1 = [];
+                        userr.subscription = arr1.concat(userr.subscription_m, userr.subscription_y);
+                        userr.save().then(()=>{
+                            users=userr;
+                            res.render('edit-students.html',{
+                                users: userr,
+                                ms:ms,
+                                mp:mp,
+                                ys:ys,
+                                yp:yp,
+                                message:'',
+                            })
+                        })
+                })      
+        })
     }else{
         res.redirect('/login');
     }
@@ -54,7 +76,6 @@ exports.activate = (req,res)=>{
 exports.sub = (req,res) => {
     var sub_m=req.body.sub_m;
     var sub_y=req.body.sub_y;
-    var i=req.body.i;
     if(sub_m){
         User.findByIdAndUpdate( 
             sid,
@@ -71,6 +92,35 @@ exports.sub = (req,res) => {
         User.findByIdAndUpdate( 
             sid,
             { $pull: { subscription_y: sub_y}},
+                (err,obb)=>{
+                    if(err){
+                        console.error(err)
+                    }
+                    res.redirect('/edit-students?id=' + sid );
+                }
+        )
+    }
+}
+
+exports.add = (req,res) =>{
+    var mis=req.body.mis;
+    var yis=req.body.yis;
+    if(ms){
+        User.findByIdAndUpdate( 
+            sid,
+            { $push: { subscription_m: mis} },
+                (err,obb)=>{
+                    if(err){
+                        console.error(err)
+                    }
+                    res.redirect('/edit-students?id=' + sid );
+                }
+            )
+        }
+    if(ys){
+        User.findByIdAndUpdate( 
+            sid,
+            { $push: { subscription_y: yis}},
                 (err,obb)=>{
                     if(err){
                         console.error(err)
--- admin-area/routers/editstudent.js
+++ admin-area/routers/editstudent.js
@@ -5,4 +5,5 @@ router.get('/',editstudentctrl.rendere);
 router.post('/sub',editstudentctrl.sub);
 router.post('/activate',editstudentctrl.activate);
 router.post('/class',editstudentctrl.save);
+router.post('/add',editstudentctrl.add);
 module.exports = router;
